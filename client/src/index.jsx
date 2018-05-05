9a73604b85c3e02b465cb62515c9461de1a19587
Julia Wong <juliawong05@gmail.com>
2018-05-04 22:04:55 -0700

    setup react environment

---
+++ client/src/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+
+
+  render() {
+    // topBar
+    // sideBar
+    // list - listComponents
+    // selected
+    return(
+      <div>
+        <h1>LogDNA</h1>
+      </div>
+      );
+  }
+
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('app'));
